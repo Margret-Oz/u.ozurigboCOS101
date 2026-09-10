7e98615419c00db7df92eafdc82816efe0e4c356
MARGRET-OZ <ugonna.ozurigbo@pau.edu.ng>
2026-09-09 22:40:11 -0700

    pracice_3

---
+++ week-3/practice_2/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let result =10;    // i32 by default
+    let age:u32 = 20;
+    let sum:f32 = 5.00 - 15.00;
+
+    println!("Result value is {} ",result);
+    println!("Age is {}",age);
+    println!("Sum is {}",sum);
+
+}
