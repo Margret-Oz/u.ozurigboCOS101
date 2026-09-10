089ded2d9a53dd8f2144af0d46e58ca8da29b038
MARGRET-OZ <ugonna.ozurigbo@pau.edu.ng>
2026-09-09 23:10:48 -0700

    practice_7

--- week-3/practice_7/src/main.rs
+++ week-3/practice_7/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let float_width_separator = 11_000.555_001;
+    println!("float value {}",float_width_separator);
+
+    let int_woth_separator = 50_000;
+    println!("int value {}",int_woth_separator);
+    
 }
